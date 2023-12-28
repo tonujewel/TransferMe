8491adebd5d0fe5fe79a97f36346dcf52204b219
Jewel Rana <tonujewel@gmail.com>
2023-12-28 19:18:26 +0600

    Initial Commit

---
+++ TransferMe/TransferMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransferMeApp.swift
+//  TransferMe
+//
+//  Created by Jewel Rana on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TransferMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
